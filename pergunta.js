18935843d4b7bc2c1fb780f8ae2667f2947db9cf
dhanielenevesk8 <dhaniele.pereira@escola.pr.gov.br>
2024-10-07 08:01:53 -0300

    v

---
+++ pergunta.js
@@ -0,0 +1,98 @@
+criacartão('Sociologia',
+    'O que é sociologia?',
+     ' Sociologia é o estudo científico das relações sociais, instituições e da sociedade.')
+    
+    criacartão('Sociologia',
+    'Quem é o fundador da sociologia?',
+      'Auguste Comte')
+    
+    criacartão(
+    'Sociologia',
+    'O que é a teoria do Positivismo?',
+      'É a ideia de que o conhecimento deve ser baseado em dados observáveis e experimentais.')
+    
+    criacartão('Sociologia',
+    'O que é um fato social, segundo Durkheim?',
+    ' É algo coercitivo, externo e geral aos indivíduos, moldando seus comportamentos')
+    
+    criacartão(
+    'Sociologia',
+    'O que significa anomia?',
+    ' É o estado de falta de normas ou desordem social que ocorre quando as regras sociais perdem a força.')
+    
+    criacartão(
+    'Sociologia',
+    'O que é a consciência coletiva?',
+    'É o conjunto de crenças, valores e normas compartilhadas por uma sociedade.')
+    
+    criacartão(
+    'Sociologia',
+    'O que é o materialismo histórico de Karl Marx?',
+    'É a teoria de que a história é movida pelas lutas de classes e pelas condições econômicas.')
+    
+    criacartão(
+    'Sociologia',
+    'O que é um modo de produção?.',
+    'É a forma como uma sociedade organiza a produção de bens e serviços, como o feudalismo ou o capitalismo.')
+    
+    criacartão(
+    'Sociologia',
+    'O que significa alienação, de acordo com Marx?.',
+    ' É o processo pelo qual o trabalhador se sente desconectado do produto de seu trabalho e do próprio processo produtivo.')
+    
+    criacartão(
+    'Sociologia',
+    'O que é ação social, segundo Max Weber?',
+     'É qualquer ação humana que tem um significado subjetivo e leva em conta a ação de outras pessoas.')
+    
+    criacartão(
+    'Sociologia',
+    'Quais são os tipos de ação social, segundo Weber?',
+     'Racional com relação a fins, racional com relação a valores, afetiva e tradicional.')
+    
+    criacartão(
+    'Sociologia',
+    'Quais são os tipos de dominação legítima, de acordo com Weber?',
+    'Tradicional, carismática e legal-racional.')
+    
+    criacartão(
+    'Sociologia',
+    'O que é função social?',
+     'É o papel que uma prática ou instituição desempenha na manutenção da sociedade.')
+    
+    criacartão(
+    'Sociologia',
+    'Qual a diferença entre funções manifestas e latentes, segundo Merton?',
+     'Funções manifestas são intencionais e visíveis, enquanto funções latentes são não intencionais e muitas vezes invisíveis.')
+    
+    criacartão(
+    'sociologia',
+    'O que é estratificação social?',
+     'É a organização hierárquica de grupos sociais baseada em critérios como riqueza, poder e prestígio.')
+    
+    criacartão(
+    'Sociologia',
+    'O que é mobilidade social?',
+     'É a capacidade de um indivíduo ou grupo de se mover de uma posição social para outra dentro de uma sociedade.')
+    
+    criacartão(
+    'Sociologia',
+    'O que é estrutura social?',
+     ' É o conjunto de relações, instituições e normas que organizam uma sociedade.')
+    
+    criacartão(
+    'Sociologia',
+    'O que é etnocentrismo?',
+     'É a tendência de julgar outras culturas com base nos valores e padrões da própria cultura.')
+    
+    criacartão(
+    'Sociologia',
+    'O que é cultura?',
+     'É o conjunto de crenças, valores, normas e práticas que são partilhadas por um grupo de pessoas.')
+    
+    criacartão(
+    'Sociologia',
+    'O que é socialização?',
+     ' É o processo pelo qual os indivíduos aprendem e internalizam as normas e valores da sociedade.')
+    
+    
